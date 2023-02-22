0b27eaaf6bb042eeca4d9dbda098119196bb6e88
okscreator <okscreator@gmail.com>
2023-02-22 03:08:17 +0300

    stocks_fix
    1. Fix Stocks page
    2. Add Stocksslider to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,18 @@ import Slider from '../components/slider/Slider'
 import Advantages from '../components/advantages/Advantages'
 import CatalogImpressions from '../components/catalogImpressions/CatalogImpressions'
 import Questions from '../components/questions/QuestionsContent'
+import Stocksslider from '../components/stocksslider/Stocksslider'
 import Sertificates from '../components/sertificates/Sertificates'
 
 const HomePage = () => {
   return (
     <>
-      <Slider/>
-      <Advantages/>
-      <CatalogImpressions/>
-      <Sertificates/>
-      <Questions/>
+      <Slider />
+      <Advantages />
+      <CatalogImpressions />
+      <Stocksslider />
+      <Sertificates />
+      <Questions />
     </>
   )
 }
